f40c92ca7434df55b040969afa7d49e1b17e4e20
TyFhoon2X3 <6710210473@psu.ac.th>
2025-03-02 15:02:19 +0700

    ss

--- cypress/e2e/Manager_473_Cypress/Manager_473.cy.js
+++ cypress/e2e/Manager_473_Cypress/Manager_473.cy.js
@@ -68,4 +68,83 @@ describe("Tracalorie - Meal Tracking", () => {
     });
 
 
+    it("delete  Costomer", () => {
+        cy.visit("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager");
+        cy.get('[ng-class="btnClass1"]').click();
+        const array = [
+            { name: "Ardee", Lastname: "Arwaehama", PostCode: 90250 },
+            { name: "Vasavat", Lastname: "kewpisai", PostCode: 90250 },
+            { name: "lukmee", Lastname: "sutanieiei", PostCode: 90250 },
+            { name: "Lukma", Lastname: "mejin", PostCode: 90250 },
+            { name: "mejai", Lastname: "maikrup", PostCode: 90250 }
+
+        ];
+
+        let i;
+        for (i = 0; i < array.length; i++) {
+            const fhoon = array[i];
+
+
+            cy.get(":nth-child(1) > .form-control").type(fhoon.name);
+            cy.get(":nth-child(2) > .form-control").type(fhoon.Lastname);
+            cy.get(":nth-child(3) > .form-control").type(fhoon.PostCode);
+
+            cy.get('form.ng-dirty > .btn').click();
+
+
+        }
+
+        cy.get('[ng-class="btnClass3"]').click();
+        cy.wait(3000);
+
+        for (let index = 0; index < 10; index++) {
+            cy.get(':nth-child(1) > :nth-child(5) > button').click()
+        }
+
+    });
+    it.only("Open Account", () => {
+        cy.visit("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager");
+        cy.get('[ng-class="btnClass1"]').click();
+    
+        const customers = [
+            { name: "Ardee", Lastname: "Arwaehama", PostCode: 90250 },
+            { name: "Vasavat", Lastname: "kewpisai", PostCode: 90250 },
+            { name: "lukmee", Lastname: "sutanieiei", PostCode: 90250 },
+            { name: "Lukma", Lastname: "mejin", PostCode: 90250 },
+            { name: "mejai", Lastname: "maikrup", PostCode: 90250 }
+        ];
+    
+        // เพิ่มลูกค้าทั้งหมด
+        for (let i = 0; i < customers.length; i++) {
+            cy.get(":nth-child(1) > .form-control").type(customers[i].name);
+            cy.get(":nth-child(2) > .form-control").type(customers[i].Lastname);
+            cy.get(":nth-child(3) > .form-control").type(customers[i].PostCode.toString());
+            cy.get('form.ng-dirty > .btn').click();
+        }
+    
+        // ไปที่หน้า "Open Account"
+        cy.get('[ng-class="btnClass2"]').click();
+    
+        // เปิดบัญชีให้ลูกค้าทุกคน
+        for (let i = 0; i < customers.length; i++) {
+            cy.get('#userSelect').select(`${customers[i].name} ${customers[i].Lastname}`);
+            cy.get('#currency').select('Dollar');
+            cy.get('form.ng-dirty > button').click();
+        }
+        cy.get('[ng-class="btnClass3"]').click();
+        cy.wait(3000); 
+
+      
+        for (let l = 0; l < customers.length; l++) {
+            const fhoon = customers[l];
+            cy.get('.form-control').type(fhoon.name);
+            cy.wait(3000); 
+            cy.get('tbody > .ng-scope > :nth-child(1)').should("have.text",fhoon.name);
+            cy.get('.form-control').clear();
+        }
+    });
+    
+
+
+
 });
